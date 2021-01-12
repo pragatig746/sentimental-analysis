7ed6d0ab999693fc51c61f3392b2ad6bb8e36092
Pragati Gupta <73426124+pragatig746@users.noreply.github.com>
2021-01-12 10:29:19 +0530

    Create sentimental analysis.py

---
+++ sentimental analysis.py
@@ -0,0 +1,15 @@
+pip install textblob
+
+from textblob import TextBlob
+
+def review(y):
+    if y<0:
+        print("Negative")
+    elif y==0:
+        print("Neutral")
+    elif y>0 and y<=1:
+        print("Positive")
+        
+x=input("Give Your Feedback: ")
+t=TextBlob(x).polarity
+review(t)
